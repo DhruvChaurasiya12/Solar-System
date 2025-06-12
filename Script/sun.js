00633488ea7b46a4c8270e122c309c54e03cfce2
DhruvChaurasiya12 <www.dhruvchaurasiya.12345@gmail.com>
2025-06-12 12:01:46 +0530

    Update sun.js

--- Script/sun.js
+++ Script/sun.js
@@ -1,4 +1,4 @@
-// Lighting
+// lighting
 const sunLight = new THREE.PointLight(0xfff8d6, 5, 500);
 sunLight.position.set(0, 0, 0);
 scene.add(sunLight);
@@ -6,13 +6,13 @@ scene.add(sunLight);
 const ambientLight = new THREE.AmbientLight(0x404040, 1.5);
 scene.add(ambientLight);
 
-// Group to hold entire solar system
+// grouping the entire system to control positions and add elements
 const solarSystem = new THREE.Group();
 scene.add(solarSystem);
 
 // Sun
 const sunTexture = loader.load('textures/sun.jpg');
-const sunSize = new THREE.SphereGeometry(20, 64, 64); // Bigger sun
+const sunSize = new THREE.SphereGeometry(20, 64, 64);
 const sunColour = new THREE.MeshBasicMaterial({ map: sunTexture });
 const sun = new THREE.Mesh(sunSize, sunColour);
 solarSystem.add(sun);
